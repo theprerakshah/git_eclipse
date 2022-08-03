2bd8005e58b6de82b9a14e03816f71622477fdf9
prerakshah <prerakshah@EZDI-LAP0146>
2022-08-03 11:31:12 +0530

    3.1 comnit

--- test/src/test/preraktest.java
+++ test/src/test/preraktest.java
@@ -5,13 +5,12 @@ public class preraktest {
     	
     	System.out.println("hello my name is prerak shah how are you");
     	String name="this is the commit i need to make";
-<<<<<<< HEAD
+
         System.out.println(name);    	
-    	
-=======
+
 System.out.println(name);    	
     	System.out.println("this is changes made on github now i need to pull this down in eclipse");
->>>>>>> branch 'master' of https://github.com/theprerakshah/git_eclipse.git
+
     	
     	
     }
