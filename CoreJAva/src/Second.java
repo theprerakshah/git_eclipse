049a851cb53394309efa49813d165c184d81d010
Yagnesh <pancholiyagnesh2001@gmail.com>
2022-08-04 10:19:05 +0530

    second added

---
+++ CoreJAva/src/Second.java
@@ -0,0 +1,31 @@
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 100;
+		short b = 23456;
+		int c = 123456789;
+		long d = 123456789;
+		float e = 3.14f;
+		boolean f = false;
+		char mychar = 'a';
+		int big = a << 1;
+		//System.out.println(a + " " + b + " " + c);
+		//System.out.println(big);
+		int x = 5>4 ? 9 : 8;
+		//System.out.println(x);
+		int [] numbers = {10, 20, 30, 40, 50};
+
+//	      for(int y : numbers ) {
+//	         System.out.print( y );
+//	       
+		//System.out.print(",");
+		System.out.println((int)((Math.random()) * 10));
+		System.out.println(" \' yagnesh \'");
+	}
+	
+	
+	}
+
+
