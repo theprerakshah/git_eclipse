02f8488cf06b2d554d99d330f2349122da6fb4ef
Yagnesh <pancholiyagnesh2001@gmail.com>
2022-09-08 11:13:25 +0530

    added

--- CoreJAva/src/LearnHashSEt.java
+++ CoreJAva/src/LearnHashSEt.java
@@ -1,13 +1,16 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
-public class LearnHashSEt {
+public class LearnHashSEt
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		// set now allow duplicate element
-		Set<Integer> set = new HashSet<>();
+		Set<Integer> set = new HashSet<Integer>();
 		//LinkedHashSet for to maintain order of elmt
 		//TreeSet for to get sorted output
-		
+
 		set.add(2);
 		set.add(2);
 		set.add(5);
@@ -16,10 +19,7 @@ public class LearnHashSEt {
 		System.out.println(set);
 		set.remove(2);
 		System.out.println(set.contains(3));
-		
-		
-	}
 
-	
+	}
 
 }
--- CoreJAva/src/StackLearning.java
+++ CoreJAva/src/StackLearning.java
@@ -1,39 +1,41 @@
-import java.util.*;
-public class StackLearning {
-	
-//	static void showpush(Stack st,int a) {
-//		st.push(new Integer(a));
-//		System.out.println("push(" + a + ")");
-//		System.out.println("stack: " + st);
-//	}
-//	static void showpop(Stack st) {
-//		int a = (int) st.pop();
-//		System.out.println(a);
-//		System.out.println("stack:" + st);
-//	}
-	public static void main(String[] args) {
+import java.util.Stack;
+
+public class StackLearning
+{
+
+	//	static void showpush(Stack st,int a) {
+	//		st.push(new Integer(a));
+	//		System.out.println("push(" + a + ")");
+	//		System.out.println("stack: " + st);
+	//	}
+	//	static void showpop(Stack st) {
+	//		int a = (int) st.pop();
+	//		System.out.println(a);
+	//		System.out.println("stack:" + st);
+	//	}
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-		Stack st = new Stack();
+		Stack<Integer> st = new Stack<>();
 		System.out.println("Stack\n" + st);
-//		showpush(st,32);
-//		showpush(st,32);
-//		showpush(st,32);
-//		showpush(st,32);
-//		showpop(st);
-//		showpop(st);
-//		showpop(st);
-//		showpop(st);
+		//		showpush(st,32);
+		//		showpush(st,32);
+		//		showpush(st,32);
+		//		showpush(st,32);
+		//		showpop(st);
+		//		showpop(st);
+		//		showpop(st);
+		//		showpop(st);
 		st.push(23);
 		st.push(43);
 		st.push(43);
-		st.push("abc");
 		System.out.println(st.pop());
 		System.out.println("Stack\n" + st);
-//		int[] arr1 = {1,32,4,5,768,565,9,2};
-//		Arrays.sort(arr1);
-//		for(int i : arr1) {
-//			System.out.println(i);
-//		}
+		//		int[] arr1 = {1,32,4,5,768,565,9,2};
+		//		Arrays.sort(arr1);
+		//		for(int i : arr1) {
+		//			System.out.println(i);
+		//		}
 	}
 
 }
