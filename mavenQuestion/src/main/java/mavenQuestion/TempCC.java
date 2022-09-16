7ad5273688540ba2d1a755fb3c4224d108405649
Yagnesh <pancholiyagnesh2001@gmail.com>
2022-09-16 10:41:13 +0530

    tempbranch first added

--- mavenQuestion/src/main/java/mavenQuestion/TempCC.java
+++ mavenQuestion/src/main/java/mavenQuestion/TempCC.java
@@ -1,34 +1,41 @@
-//package mavenQuestion;
-//
-//import java.util.Scanner;
-//
-//public class TempCC
-//{
-//
-//	public static void main(String[] args)
-//	{
-//		// TODO Auto-generated method stub
-//
-//		Scanner sc = new Scanner(System.in);
-//		int t = sc.nextInt();
-//		while(t-- > 0)
-//		{
-//			// your code goes here
-//			int a = sc.nextInt();
-//			int b = sc.nextInt();
-//			int c = sc.nextInt();
-//			int d;
-//			
-//			if(d >= a)
-//			{
-//				System.out.println("Qualify");
-//			}
-//			else
-//			{
-//				System.out.println("notqualify");
-//			}
-//		}
-//
-//	}
-//
-//}
+package mavenQuestion;
+
+import java.util.Scanner;
+
+public class TempCC
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		//		int m = sc.nextInt();
+		//		int n1 = (int) Math.ceil(n / 10);
+		//		int m1 = (int) Math.ceil(m / 10);
+		//		int ans = Math.abs(n1 - m1);
+		//		System.out.println(ans);
+		float f = n / 10;
+		System.out.println(f);
+		//		Scanner sc = new Scanner(System.in);
+		//		int t = sc.nextInt();
+		//		while(t-- > 0)
+		//		{
+		//			// your code goes here
+		//			int a = sc.nextInt();
+		//			int b = sc.nextInt();
+		//			int c = sc.nextInt();
+		//			int d;
+		//			
+		//			if(d >= a)
+		//			{
+		//				System.out.println("Qualify");
+		//			}
+		//			else
+		//			{
+		//				System.out.println("notqualify");
+		//			}
+		//		}
+		//
+	}
+
+}
