42cb1013b039dd63a0445036b713e0b57d102ad9
Yagnesh <pancholiyagnesh2001@gmail.com>
2022-09-22 17:31:15 +0530

    tempCC added

--- mavenQuestion/src/main/java/mavenQuestion/TempCC.java
+++ mavenQuestion/src/main/java/mavenQuestion/TempCC.java
@@ -1,33 +1,123 @@
 package mavenQuestion;
 
-import java.util.Scanner;
-
 public class TempCC
 {
 
 	public static void main(String[] args)
 	{
+		int a = Integer.MAX_VALUE;
+		System.out.println(a);
+
+		//		Scanner sc = new Scanner(System.in);
+		//		int t = sc.nextInt();
+		//		while(t-- > 0)
+		//		{
+		//			long N = sc.nextLong();
+		//			long M = sc.nextLong();
+		//			int[] arr = new int[(int) N];
+		//			int[] ansarr = new int[(int) N];
+		//			for(int i = 0; i < N; i++)
+		//			{
+		//				arr[i] = sc.nextInt();
+		//			}
+		//			for(int i = 0; i < N; i++)
+		//			{
+		//				if(Math.abs(arr[i] - 1) >= Math.abs(arr[i] - M))
+		//				{
+		//					ansarr[i] = 1;
+		//				}
+		//				else
+		//				{
+		//					ansarr[i] = (int) M;
+		//				}
+		//			}
+		//			int ans = 0;
+		//			for(int i = 0; i < N; i++)
+		//			{
+		//				ans += Math.abs(arr[i] - ansarr[i]);
+		//			}
+		//			System.out.println(ans);
+		//		}
+
 		/* package codechef; // don't place package name! */
 
 		/* Name of the class has to be "Main" only if the class is public. */
 
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		while(t-- > 0)
-		{
-			// your code goes here
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int c = sc.nextInt();
-			if((a + b + c) == 180)
-			{
-				System.out.println("YES");
-			}
-			else
-			{
-				System.out.println("No");
-			}
-		}
+		//				
+		//				long n = sc.nextLong();
+		//		int t = sc.nextInt();
+		//		while(t --> 0){
+		//		    // your code goes here
+		//		    int[] arr = new int[10];
+		//		    for(int i=0 ; i<10 ;i++){
+		//		        arr[i] = sc.nextInt();
+		//		    }
+		//		    int team1goal = 0,team2goal = 0;
+		//		    for(int i =0; i < n ;i++){
+		//		        if(i%2 == 0 && arr[i] == 1){
+		//		            team1goal++;
+		//		        }
+		//		        if(i%2 != 0 && arr[i] == 1){
+		//		            team2goal++;
+		//		        }
+		//		    }
+		//		    if(team1goal > team2goal){
+		//		        System.out.println("1");
+		//		    }else{
+		//		        System.out.println("2");
+		//		    }
+		//		}
+
+		//		Scanner sc = new Scanner(System.in);
+		//		int t = sc.nextInt();
+		//		while(t-- > 0)
+		//		{
+		//			int N = sc.nextInt();
+		//			int A = sc.nextInt();
+		//			int B = sc.nextInt();
+		//			int[] arr = new int[N];
+		//			for(int i = 0; i < N; i++)
+		//			{
+		//				arr[i] = sc.nextInt();
+		//			}
+		//			double acount = 0.000000, bcount = 0.000000;
+		//			for(int i = 0; i < N; i++)
+		//			{
+		//				if(arr[i] == A)
+		//				{
+		//					acount = acount + 1.00000;
+		//				}
+		//				if(arr[i] == B)
+		//				{
+		//					bcount = bcount + 1.00000;
+		//				}
+		//			}
+		//			double ans = (acount / (double) N * bcount / (double) N);
+		//			System.out.println(ans);
+
+		//		Set<Integer> myset = new HashSet<Integer>();
+		//		myset.add(2010);
+		//		myset.contains(1050);
+		//		int h = 0, x = 0, y = 0;
+		//		int ans = (int) (h * (x + Math.floor(1 / 2)));
+		//		Scanner sc = new Scanner(System.in);
+		//		String d = sc.next();
+		//		int t = sc.nextInt();
+		//		while(t-- > 0)
+		//		{
+		//			// your code goes here
+		//			int a = sc.nextInt();
+		//			int b = sc.nextInt();
+		//			int c = sc.nextInt();
+		//			if((a + b + c) == 180)
+		//			{
+		//				System.out.println("YES");
+		//			}
+		//			else
+		//			{
+		//				System.out.println("No");
+		//			}
+		//		}
 
 		//		Scanner sc = new Scanner(System.in);
 		//		int n = sc.nextInt();
